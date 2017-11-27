04217e26b959ee69072e7005849ee89893c45c85
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2017-11-27 16:46:52 +0100

    updated compilation for WINDOWS

--- sp-compile-all.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@rmdir /S GC ; git clone --depth 1 git@github.com:ebertolazzi/GenericContainer.git GC
-@pushd win_compile
-@call sp-compile-vs2013
-@call sp-compile-vs2015
-@call sp-install-vs2013
-@call sp-install-vs2015
-@popd
--- win_compile/sp-setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-@SET BASE=C:\MechatronixBuild
-@SET LIBDIR=%BASE%\lib
-@SET INCDIR=%BASE%\include
-
-@if not exist C:\MechatronixBuild mkdir C:\MechatronixBuild
-@if not exist %LIBDIR% mkdir %LIBDIR%
-@if not exist %INCDIR% mkdir %INCDIR%
