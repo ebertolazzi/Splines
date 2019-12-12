32d8dc2f10711286f84e4db0001ca69873190cb5
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2019-12-12 13:30:28 +0100

    updated Rakefile_conf.rb

--- Rakefile_conf.rb
+++ Rakefile_conf.rb
@@ -6,13 +6,19 @@ else
   COMPILE_DYNAMIC    = false
   COMPILE_EXECUTABLE = false
 
-  cmakeversion = %x( cmake --version ).scan(/\d+\.\d+/).last
-  mm = cmakeversion.split('.');
-  if mm[0].to_i > 3 || (mm[0].to_i == 3 && mm[1].to_i >= 12) then
-    PARALLEL = '--parallel 8 '
-    QUIET    = '-- --quiet '
-  else
+  case RUBY_PLATFORM
+  when /mingw|mswin/
     PARALLEL = ''
     QUIET    = ''
+  else
+    cmakeversion = %x( cmake --version ).scan(/\d+\.\d+/).last
+    mm = cmakeversion.split('.');
+    if mm[0].to_i > 3 || (mm[0].to_i == 3 && mm[1].to_i >= 12) then
+      PARALLEL = '--parallel 8 '
+      QUIET    = '-- --quiet '
+    else
+      PARALLEL = ''
+      QUIET    = ''
+    end
   end
 end
