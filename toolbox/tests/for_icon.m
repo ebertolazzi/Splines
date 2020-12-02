f9127503f54a74d0e3281d4f540f6cb82190b6e3
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2020-12-02 22:04:26 +0100

    updated icon and toolbox

--- toolbox/tests/for_icon.m
+++ toolbox/tests/for_icon.m
@@ -19,12 +19,21 @@
 
 X = 0:0.1:1;
 Y = 0:0.25:2;
+
 [XX,YY] = ndgrid(X,Y);
-ZZ = franke(XX,YY);
+
+term1 = 0.75 * exp(-(9*XX-2).^2/4 - (9*YY-2).^2/4);
+term2 = 0.75 * exp(-(9*XX+1).^2/49 - (9*YY+1)./10);
+term3 = 0.5 * exp(-(9*XX-7).^2/4 - (9*YY-3).^2/4);
+term4 = -0.2 * exp(-(9*XX-4).^2 - (9*YY-7).^2);
+
+ZZ = term1 + term2 + term3 + term4;
 
 ak = Spline2D('akima',X,Y,ZZ);
 
-surf(XX,YY,ZZ), view(145,-2), set(gca,'Fontsize',16);
+surf(XX,YY,ZZ);
+view(145,-2);
+set(gca,'Fontsize',16);
 
 X = 0:0.01:1;
 Y = 0:0.01:2;
