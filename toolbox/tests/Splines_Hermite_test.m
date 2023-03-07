54a5ff3ec791bb8e825a781890c2959b907c5071
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2023-03-07 17:02:32 +0100

    correct Hermite class (call Mex)

---
+++ toolbox/tests/Splines_Hermite_test.m
@@ -0,0 +1,38 @@
+%%-------------------------------------------------------------------------%%
+%%                                                                         %%
+%%  Copyright (C) 2020                                                     %%
+%%                                                                         %%
+%%         , __                 , __                                       %%
+%%        /|/  \               /|/  \                                      %%
+%%         | __/ _   ,_         | __/ _   ,_                               %%
+%%         |   \|/  /  |  |   | |   \|/  /  |  |   |                       %%
+%%         |(__/|__/   |_/ \_/|/|(__/|__/   |_/ \_/|/                      %%
+%%                           /|                   /|                       %%
+%%                           \|                   \|                       %%
+%%                                                                         %%
+%%      Enrico Bertolazzi                                                  %%
+%%      Dipartimento di Ingegneria Industriale                             %%
+%%      Universita` degli Studi di Trento                                  %%
+%%      email: enrico.bertolazzi@unitn.it                                  %%
+%%                                                                         %%
+%%-------------------------------------------------------------------------%%
+
+H = BaseHermite();
+
+t = 0:1/1000:1;
+
+[h0,h1,h2,h3] = H.base( t );
+subplot(2,2,1);
+plot( t, h0, t, h1, t, h2, t, h3 );
+
+[h0,h1,h2,h3] = H.base_D( t );
+subplot(2,2,2);
+plot( t, h0, t, h1, t, h2, t, h3 );
+
+[h0,h1,h2,h3] = H.base_DD( t );
+subplot(2,2,3);
+plot( t, h0, t, h1, t, h2, t, h3 );
+
+[h0,h1,h2,h3] = H.base_DDD( t );
+subplot(2,2,4);
+plot( t, h0, t, h1, t, h2, t, h3 );
